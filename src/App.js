e81ba9e6c21368d7830f5b3cffcc508a193570bd
Ruslan Zamalov <zamruma@gmail.com>
2020-10-29 23:49:12 +0300

    social network frame

--- src/App.js
+++ src/App.js
@@ -3,39 +3,45 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <Technologies/>
-    </div>
+    <main className="wrapper">
+      <header className="header">
+        <div className="header__logo">
+          <a href="#" className="header__link">Service</a>
+        </div>
+      </header>
+      <aside className="nav">
+        <nav className="nav__links">
+          <ul className="nav__items">
+            <li className="nav__item">
+              <a href="#">
+                Profile
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#">Settings</a>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <div className="content">
+        <div>
+          <img src="https://www.donnashape.com/33840-medium_default/hot-girl-body.jpg" alt="hot-girl"/>
+        </div>
+        <div>
+          <img src="https://sc02.alicdn.com/kf/HTB1yA_abXzsK1Rjy1Xbq6xOaFXaA.jpg" alt="hot-girl"/>
+        </div>
+        <div>
+          my Post
+          <div>
+            new post
+          </div>
+          <div>
+            post one
+          </div>
+        </div>
+      </div>
+    </main>
   );
-}
-
-const Header = () => {
-  return (
-    <div>
-      <a href='#'>Home</a>
-      <a href='#'>Home</a>
-      <a href='#'>Home</a>
-    </div>
-  )
-}
-
-const Technologies = () => {
-  return (
-    <div className="App">
-      <ul>
-        <li>
-          Css
-        </li>
-        <li>
-          HTML
-        </li>
-        <li>
-          JS
-        </li>
-      </ul>
-    </div>
-  )
 }
 
 export default App;
