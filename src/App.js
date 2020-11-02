6eafeae7b59bf60f177fdf429a599f943edbab45
Ruslan Zamalov <zamruma@gmail.com>
2020-11-02 14:52:30 +0300

    Step 19 add react-routing-dom and new page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Nav/Nav';
 import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 const App = () => {
   return (
-    <main className="wrap">
-      <Header/>
-      <Navbar/>
-      <div className="wrap__content">
-        {/*<Profile/>*/}
-        <Dialogs/>
-      </div>
-    </main>
+    <Router>
+      <main className="wrap">
+        <Header/>
+        <Navbar/>
+        <div className="wrap__content">
+          <Route path="/profile" component={Profile} />
+          <Route path="/messages" component={Dialogs} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+        </div>
+      </main>
+    </Router>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,33 @@
+import React from 'react';
+
 const Navbar = () => {
   return (
     <nav className="nav">
       <ul className="nav__items">
         <li className="nav__item">
-          <a href="#">
+          <a href="/profile">
             Profile
           </a>
         </li>
         <li className="nav__item">
-          <a href="#">Settings</a>
+          <a href="/messages">
+            Messages
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/news">
+            News
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/music">
+            Music
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/settings">
+            Settings
+          </a>
         </li>
       </ul>
     </nav>
