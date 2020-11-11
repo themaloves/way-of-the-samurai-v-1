5d1097e84c032f6686df5e7c955f44bd84883c1d
Ruslan Zamalov <zamruma@gmail.com>
2020-11-12 00:29:56 +0300

    Step 52 Side Effect

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,16 +3,18 @@ import Axios from 'axios';
 import userPhoto from '../../assets/img/ava-default.jpg';
 
 const Users = (props) => {
-
-  if (props.users.length === 0) {
-    Axios.get('https://social-network.samuraijs.com/api/1.0/users')
-      .then(res => {
-        props.setUsers(res.data.items);
-      });
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      Axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(res => {
+          props.setUsers(res.data.items);
+        });
+    }
   }
 
   return (
     <section>
+      <button onClick={getUsers}>Get Users</button>
       {
         props.users.map(u => <div className="users" key={u.id}>
         <span>
@@ -20,9 +22,10 @@ const Users = (props) => {
             <img className="users__img" src={u.photos.small !== null ? u.photos.small : userPhoto} alt="ava"/>
           </div>
           <div>
-            {u.followed ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> :
-              <button onClick={() => {props.follow(u.id)}}>Follow</button>}
-
+            {
+              u.followed ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> :
+              <button onClick={() => {props.follow(u.id)}}>Follow</button>
+            }
           </div>
         </span>
           <span>
