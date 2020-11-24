ef893d51930f0f0d727e608002e54b51d29fc3fd
Ruslan Zamalov <zamruma@gmail.com>
2020-11-24 10:11:33 +0300

    Step 74 componentDidUpdate

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -10,7 +10,7 @@ class ProfileContainer extends React.Component {
   componentDidMount() {
     let userId = this.props.match.params.userId;
     if (!userId) {
-      userId = 2;
+      userId = 12643;
     }
     this.props.getUserProfile(userId);
     this.props.getStatus(userId)
